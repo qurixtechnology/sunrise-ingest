a868af0764af185ce0f1f9ee7b061251c6aeff88
Fernando Zepeda <fernando.zepeda@qurix.tech>
2022-05-13 21:26:00 +0200

    add new users with sql

---
+++ sunrise-mvp/storage_layer/create_azure_ad_logins_db.sql
@@ -0,0 +1,10 @@
+-- Create Azure AD users: needed after deployment with Terraform
+CREATE USER [fernando.zepeda@qurix.tech] FROM EXTERNAL PROVIDER;
+
+-- Grant roles
+ALTER ROLE db_datareader ADD MEMBER [fernando.zepeda@qurix.tech]; 
+ALTER ROLE db_datawriter ADD MEMBER [fernando.zepeda@qurix.tech]; 
+ALTER ROLE db_ddladmin ADD MEMBER [fernando.zepeda@qurix.tech]; 
+
+--Grant Database Permissions
+GRANT VIEW DATABASE STATE TO [fernando.zepeda@qurix.tech]
