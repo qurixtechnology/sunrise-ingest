43aa26a29bd316af16e04971b61d8d58f087e8c6
Fernando Zepeda <fernando.zepeda@qurix.tech>
2022-05-22 22:43:05 +0200

    format

--- sunrise/variables.tf
+++ sunrise/variables.tf
@@ -1,74 +1,74 @@
-variable use_case {
-    type = string
-    description = "The use case for which the resource group will be used for."
+variable "use_case" {
+  type        = string
+  description = "The use case for which the resource group will be used for."
 }
 
-variable environment {
-    type = string
-    description = "dev, test, prod"
+variable "environment" {
+  type        = string
+  description = "dev, test, prod"
 
-    validation {
-        condition = anytrue([
-            var.environment == "dev",
-            var.environment == "test",
-            var.environment == "prod",  
-        ])
-        error_message = "Environment must be 'dev', 'test' or 'prod'."
-    }
+  validation {
+    condition = anytrue([
+      var.environment == "dev",
+      var.environment == "test",
+      var.environment == "prod",
+    ])
+    error_message = "Environment must be 'dev', 'test' or 'prod'."
+  }
 }
 
-variable location {
-    type = string
-    description = "Azure region"
-    default = "West Europe"
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "West Europe"
 
-    validation {
-        condition = anytrue([
-            var.location == "West Europe",
-            var.location == "Germany West Central",
-            var.location == "Germany North",
-            var.location == "Germany",
-        ])
-        error_message = "Location is not valid."
-    }
+  validation {
+    condition = anytrue([
+      var.location == "West Europe",
+      var.location == "Germany West Central",
+      var.location == "Germany North",
+      var.location == "Germany",
+    ])
+    error_message = "Location is not valid."
+  }
 }
 
-variable service_principal_id {
-    type = string
-    description = "Id (appId) of the service principal."
+variable "service_principal_id" {
+  type        = string
+  description = "Id (appId) of the service principal."
 }
 
-variable service_principal_secret {
-    type = string
-    description = "Secret of the service principal."
+variable "service_principal_secret" {
+  type        = string
+  description = "Secret of the service principal."
 }
 
-variable subscription_id {
-    type = string
-    description = "Azure Subscription Id."
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription Id."
 }
 
-variable tenant_id {
-    type = string
-    description = "Azure tenant id."
-} 
-
-variable azad_admin_login {
-    type = string
-    description = "Azure AD admin login."
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant id."
 }
 
-variable azad_admin_object_id {
-    type = string
-    description = "Azure AD admin object id."
+variable "azad_admin_login" {
+  type        = string
+  description = "Azure AD admin login."
 }
 
-variable aks_ssh_key {
-    type = string
-    default = "ssh public key for AKS."
-    sensitive = true 
+variable "azad_admin_object_id" {
+  type        = string
+  description = "Azure AD admin object id."
 }
 
-variable aks_dns_prefix {
-    type = string
+variable "aks_ssh_key" {
+  type      = string
+  default   = "ssh public key for AKS."
+  sensitive = true
+}
+
+variable "aks_dns_prefix" {
+  type = string
 }
