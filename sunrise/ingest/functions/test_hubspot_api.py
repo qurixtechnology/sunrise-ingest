c3e4abd6e035523c4727d85199377f995e09f042
Fernando Zepeda <fernando.zepeda@qurix.tech>
2022-06-28 20:24:06 +0200

    hide local scripts

--- sunrise/ingest/functions/test_hubspot_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pprint
-from configs.settings import config
-
-response = requests.get(
-    f"https://api.hubapi.com/crm/v3/objects/notes?hapikey={config.HUBSPOT_API_KEY}"
-)
-pprint.pprint(response.json())
